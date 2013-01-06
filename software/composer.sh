779d840fbfd74ada1e8318469f297ce224bdd022
Aaron Navarro <marxianu@gmail.com>
2013-01-06 15:54:58 +0100

    Composer installer now informs for composer already installed.

--- software/composer.sh
+++ software/composer.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 function installComposer() {
-	cwd=`pwd`
 	tmp_dir="/tmp/composer-$$"
 	mkdir -p $tmp_dir
 	cd $tmp_dir
 	curl -s http://getcomposer.org/installer | php
 	sudo mv composer.phar /usr/local/bin/composer
-	cd $cwd
 	rm -rf $tmp_dir
 }
+
 if [ ! -e /usr/local/bin/composer ]; then
 	installComposer
+else
+	echo "Composer already installed. Nothing changed."
 fi
