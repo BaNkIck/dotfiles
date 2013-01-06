dd09ea61aaea5f129e9009aa5fd8884bab5fe907
Aaron Navarro <marxianu@gmail.com>
2013-01-06 12:05:03 +0100

    Do not update nor install adittional commands if brew installation fails

--- software/brew.sh
+++ software/brew.sh
@@ -2,33 +2,37 @@
 
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 
-# Make sure we’re using the latest Homebrew
-brew update
+if [ $? -eq 0 ]; then
 
-# Upgrade any already-installed formulae
-brew upgrade
+	# Make sure we’re using the latest Homebrew
+	brew update
 
-brew install bash-completion git
+	# Upgrade any already-installed formulae
+	brew upgrade
 
-# Install GNU core utilities (those that come with OS X are outdated)
-# brew install coreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-# brew install findutils
-# Install Bash 4
-# brew install bash
-# Install wget
-brew install wget
+	brew install bash-completion git
 
-# Remove outdated versions from the cellar
-brew cleanup
+	# Install GNU core utilities (those that come with OS X are outdated)
+	# brew install coreutils
+	# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+	# brew install findutils
+	# Install Bash 4
+	# brew install bash
+	# Install wget
+	brew install wget
 
-# cd
-# git clone git://github.com/sstephenson/rbenv.git .rbenv
-# mkdir -p ~/.rbenv/plugins
-# cd ~/.rbenv/plugins
-# git clone git://github.com/sstephenson/ruby-build.git
-# export PATH="$HOME/.rbenv/bin:$PATH"
-# rbenv rehash
-# rbenv install 1.9.3-p194
-# rbenv rehash
-# rbenv global 1.9.3-p194
+	# Remove outdated versions from the cellar
+	brew cleanup
+
+	# cd
+	# git clone git://github.com/sstephenson/rbenv.git .rbenv
+	# mkdir -p ~/.rbenv/plugins
+	# cd ~/.rbenv/plugins
+	# git clone git://github.com/sstephenson/ruby-build.git
+	# export PATH="$HOME/.rbenv/bin:$PATH"
+	# rbenv rehash
+	# rbenv install 1.9.3-p194
+	# rbenv rehash
+	# rbenv global 1.9.3-p194
+
+fi
