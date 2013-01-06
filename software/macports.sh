a7e11ffd2cf2f4e1a047a71b4750e7895ffac46d
Aaron Navarro <marxianu@gmail.com>
2013-01-06 15:56:02 +0100

    Added MacPorts installer

---
+++ software/macports.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function installMacPorts() {
+	URL=`curl -s http://www.macports.org/install.php | grep -Eo "<a [^>]*>tar\.gz</a>" | sed -E "s/<a [^>]*href=['\"]([^'\"]+)['\"][^>]*>tar\.gz<\/a>/\1/" | sed 's/ /%20/g'`
+	DOWNLOAD_DIR="/tmp/macports-$$"
+	FILE="${URL##http*/}"
+	mkdir -p "$DOWNLOAD_DIR"
+	cd "$DOWNLOAD_DIR"
+	sudo -v
+	curl -O "$URL" && tar xzf "$FILE" && cd "${FILE%.tar.gz}" && ./configure && make && sudo make install
+	rm -rf "$DOWNLOAD_DIR"
+	sudo /opt/local/bin/port -v selfupdate
+}
+
+if [ ! -f "/opt/local/bin/port" ]; then
+	installMacPorts
+else
+	echo "MacPorts already installed. Nothing changed."
+fi
