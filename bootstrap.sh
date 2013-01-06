24e8c32b3742ba2c839c9951bba6343f2529456c
Aaron Navarro <marxianu@gmail.com>
2013-01-06 12:13:38 +0100

    Some comments added to bootstrap.sh file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+# cd to the script directory
 cd "$(dirname "$0")"
+
+# get `dotfiles` last version
 git pull
+
 function doIt() {
+
+    # Install PHP 5.4
     if [ ! -e /usr/local/php5 ]; then
         read -p "Install PHP 5.4? (y/n) " -n 1
         echo
@@ -9,13 +16,18 @@ function doIt() {
             curl -s http://php-osx.liip.ch/install.sh | bash -s 5.4
         fi
     fi
+
+    # Copy all files to home directory
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude ".gitconfig" --exclude "bootstrap.sh" --exclude "README.md" --exclude "init" --exclude "data" --exclude "software" -av . ~
+
+    # Hanlde .extra file
     if [ -f ~/.extra ]; then
         echo "These changes have been made in the .extra.dist file, please handle these manually"
         diff .extra.dist ~/.extra
     else
         mv ~/.extra.dist ~/.extra
     fi
+
     # Handle .gitconfig file
     if [ -f ~/.gitconfig ] && ! grep -q "\!\!\! dotfiles .gitconfig included \!\!\!" ~/.gitconfig; then
         cat .gitconfig > ~/.gitconfig_tmp
@@ -26,6 +38,8 @@ function doIt() {
     elif [ ! -f ~/.gitconfig ]; then
         cp .gitconfig ~/.gitconfig
     fi
+
+    # Handle Sublime Text 2 files
     # if [ -d ~/Library ]; then
     #     if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages-old ]; then
     #         mv ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages-old
@@ -37,6 +51,8 @@ function doIt() {
     #     fi
     # fi
 }
+
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     doIt
 else
@@ -46,5 +62,8 @@ else
         doIt
     fi
 fi
+
 unset doIt
+
+# load all changes
 source ~/.bash_profile
